6adab2703aa517c07589ac45823084204a598816
Dybido <dybido93@gmail.com>
2016-06-27 22:58:55 +1000

    Consonants only read up to the first character. Vowels work as supposed to.

--- src/PigLatin.java
+++ src/PigLatin.java
@@ -1,4 +1,35 @@
+import java.util.*;
+import java.io.*;
 
 public class PigLatin {
-
+	
+	public static void main(String[] argc){
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextLine()){
+			String in = sc.nextLine().toString();
+			String letter = in.substring(0, 1);
+			if(isVowel(letter)){
+				String yay = "yay";
+				in = in + yay;
+				System.out.println(in);
+			}else{
+				letter = letter + "ay";
+				String encrypted = in.substring(1, in.length());
+				encrypted = encrypted + letter;
+				System.out.println(encrypted);
+			}
+		}
+		sc.close();
+	}
+	
+	public static boolean isVowel(String s){
+		String[] vowels = {"a" ,"e", "i", "o", "u"};
+		for(String c : vowels){
+			if(c.equals(s)){
+				return true;
+			}
+		}
+		return false;
+	}
 }
